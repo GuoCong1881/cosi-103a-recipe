b52b5eda4a34aa4d6fa65060bc9fd4b175b8afed
Cong Guo <61178146+GuoCong1881@users.noreply.github.com>
2024-01-23 04:08:34 +0000

    tictactoe tutorial part 4 - calcualte the winner - irene guo

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -11,9 +11,15 @@ function Square({ value, onSquareClick }){
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true); 
-
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext? "X": "O");
+  }
   function handleClick(i){
-    if (squares[i]){
+    if (squares[i] || calculateWinner(squares)){
       return;
     }
     const nextSquares = squares.slice();
@@ -27,6 +33,7 @@ export default function Board() {
   }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -44,4 +51,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares){
+  const lines =[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i ++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
