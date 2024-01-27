4220c721bc8c2106b7ab1dcafb0bf197312b6b1d
Emily <156863224+emilyjwohgemuth@users.noreply.github.com>
2024-01-27 22:05:40 +0000

    adjusted styling

--- krecipe/src/App.js
+++ krecipe/src/App.js
@@ -20,7 +20,7 @@ function RecipeBoard({ recipes }) {
 }
 
 function TopRecipes() {
-  return <h2 className="top-recipes">All Of Our Recipes</h2>;
+  return <h2 className="top-recipes">Today's Top Recipes</h2>;
 }
 
 function Recipe({ recipe }) {
@@ -240,7 +240,6 @@ function App() {
   return (
     <div>
       <Header />
-      <RecipeBoard recipes={recipes} />
       <TopRecipes />
       <RecipeList recipes={recipes} />
     </div>
