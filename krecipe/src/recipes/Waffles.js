1d868d33081b2842068b6b8e9b936f025b1aa4e2
jruet <156863061+jruet@users.noreply.github.com>
2024-01-31 21:42:18 +0000

    Fleshed out styling for the recipe template

--- krecipe/src/recipes/Waffles.js
+++ krecipe/src/recipes/Waffles.js
@@ -5,24 +5,28 @@ import './allrecipes.css';
 const Waffles = () => {
     return (
         <div className="recipe">
-            <img src="waffles.jpg" alt="Waffles" className="recipe-image" />
-            <h1 className="recipe-title">Waffles</h1>
-            <h2 className="recipe-heading">Description</h2>
-            <p className="recipe-description">Insert your description here</p>
-            <h2 className="recipe-heading">Ingredients</h2>
-            <ul className="recipe-list">
-                <li>Ingredient 1</li>
-                <li>Ingredient 2</li>
-                <li>Ingredient 3</li>
-                {/* Add more ingredients as needed */}
-            </ul>
-            <h2 className="recipe-heading">Instructions</h2>
-            <ol className="recipe-list">
-                <li>Step 1</li>
-                <li>Step 2</li>
-                <li>Step 3</li>
-                {/* Add more steps as needed */}
-            </ol>
+            <div className="recipe-top">
+                <img src="waffles.jpg" alt="Waffles" className="recipe-image" />
+                <div className="recipe-vitals">
+                    <h1 className="recipe-title">Belgian Waffles</h1>
+                    <h2 className="recipe-heading">Description</h2>
+                    <p className="recipe-description">Insert your description here</p>
+                </div>
+            </div>
+                <h2 className="recipe-heading">Ingredients</h2>
+                <ul className="recipe-list">
+                    <li>Ingredient 1</li>
+                    <li>Ingredient 2</li>
+                    <li>Ingredient 3</li>
+                    {/* Add more ingredients as needed */}
+                </ul>
+                <h2 className="recipe-heading">Instructions</h2>
+                <ol className="recipe-list">
+                    <li>Step 1</li>
+                    <li>Step 2</li>
+                    <li>Step 3</li>
+                    {/* Add more steps as needed */}
+                </ol>
         </div>
     );
 };
