6a0a4dd5a71a24baa875cbcd8a73f1f185a06fe3
jruet <156863061+jruet@users.noreply.github.com>
2024-02-29 20:31:26 +0000

    commited additional layout structures

--- backend/index.test.js
+++
@@ -1,7 +0,0 @@
-import request from 'supertest';
-import app from 'index.js';
-
-it ('responds to /', async () => {
-    const response = await request(app).get('/');
-    expect(response.text).toMatch('Hello Class, Again!');
-    });
