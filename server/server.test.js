c0d9fca054ef7bac6a66f8f4d3b9b2abe7a221c4
pranav-praburam <ppraburam@brandeis.edu>
2024-04-15 12:42:29 -0400

    server testing

--- server/server.test.js
+++ server/server.test.js
@@ -1,45 +1,13 @@
 const request = require('supertest');
-const express = require('express');
-const app = express();
+const server = require('../server');
 
-// Mock your recipes array
-const recipes = [
-  {
-    name: 'Test Recipe',
-    ingredients: 'Test Ingredients',
-    instructions: 'Test Instructions'
-  }
-];
-
-// Import your server.js file
-const server = require('C:\\Users\\15088\\OneDrive\\Desktop\\vsCode\\cosi-103a\\server\\server.js');
-
-describe('Test the root path', () => {
-  test('It should response the GET method', async () => {
-    const response = await request(server).get('/api/recipes');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(recipes);
-  });
-
-  test('It should response the POST method', async () => {
-    const newRecipe = {
-      name: 'New Test Recipe',
-      ingredients: 'New Test Ingredients',
-      instructions: 'New Test Instructions'
-    };
-    const response = await request(server).post('/api/recipes').send(newRecipe);
-    expect(response.statusCode).toBe(201);
-    expect(response.body).toEqual(newRecipe);
-  });
-
-  test('It should return 400 for invalid recipe format', async () => {
-    const invalidRecipe = {
-      name: 'Invalid Test Recipe',
-      ingredients: 'Invalid Test Ingredients'
-      // Missing instructions
-    };
-    const response = await request(server).post('/api/recipes').send(invalidRecipe);
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toEqual({ message: 'Invalid recipe format' });
+describe('Server', function() {
+  it('should list ALL blobs on /blobs GET', function(done) {
+    request(server)
+      .get('/blobs')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
   });
 });
